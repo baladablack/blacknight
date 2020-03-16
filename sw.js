30fe08eb1e938bca9b8e2d180d73167198f76b8b
Euques Wabes <euqueswabes@iMac-de-Euques.local>
2020-03-16 13:19:56 -0300

    off

--- sw.js
+++ sw.js
@@ -2,44 +2,19 @@ self.addEventListener('fetch', () => {
   // literally does nothing
 });
 
-//console.info('SW for test running');
-importScripts("https://cdn.pushalert.co/sw-18778.js");
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
-  );
+importScripts('https://cdn.ampproject.org/sw/amp-sw.js');
+AMP_SW.init({
+  assetCachingOpitions; [
+    {regexp:/\.(png|jpg|svg|webp)/,
+    cachingSrrategy: 'CACHE_FIRST'
+  },
+  {
+    regexp:/\.(js)/,
+    cachingSrrategy: 'STALE_WHILE_REVALIDADTE'
+  }
+  ],
+  offilinePageOptions: {
+    url: '/offline.html',
+    assets:[]
+  }
 });
-
-// CODELAB: Update cache names any time any of the cached files change.
-const FILES_TO_CACHE = [
-  './offline.html',
-];
-
-// CODELAB: Precache static resources here.
-evt.waitUntil(
-  caches.open(CACHE_NAME).then((cache) => {
-    console.log('[ServiceWorker] Pre-caching offline page');
-    return cache.addAll(FILES_TO_CACHE);
-  })
-);
-
-// CODELAB: Remove previous cached data from disk.
-evt.waitUntil(
-  caches.keys().then((keyList) => {
-    return Promise.all(keyList.map((key) => {
-      if (key !== CACHE_NAME) {
-        console.log('[ServiceWorker] Removing old cache', key);
-        return caches.delete(key);
-      }
-    }));
-  })
-);
