014e768ae231e11d80253bb6ef4b0792e1add124
Euques Wabes <euqueswabes@iMac-de-Euques.local>
2020-03-17 17:11:05 -0300

    add

--- sw.js
+++ sw.js
@@ -2,21 +2,45 @@ self.addEventListener('fetch', () => {
   // literally does nothing
 });
 
-        var CACHE_NAME = 'static-v1';
 
-        self.addEventListener('install', function (event) {
-          event.waitUntil(
-            caches.open(CACHE_NAME).then(function (cache) {
-              return cache.addAll([
-                '/',
-                `/assets/images/image01.png`,
-                `/assets/main.css`,
-                `/assets/main.js`,
-                `/scripts/main.min.js`,
-              ]);
-            })
-          )
-        });
+var CACHE_NAME = 'static-v1';
+
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll([
+        '/',
+        `/assets/images/image01.png`,
+        `/assets/main.css`,
+        `/assets/main.js`,
+        `/scripts/main.min.js`,
+      ]);
+    })
+  )
+});
+
+self.addEventListener('activate', function activator(event) {
+  event.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(keys
+        .filter(function (key) {
+          return key.indexOf(CACHE_NAME) !== 0;
+        })
+        .map(function (key) {
+          return caches.delete(key);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (cachedResponse) {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
 
 importScripts('https://cdn.ampproject.org/sw/amp-sw.js');
 AMP_SW.init({
