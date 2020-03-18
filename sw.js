c7b9121c8de2819a64a082e69b10068a638bb60e
Euques Wabes <euqueswabes@iMac-de-Euques.local>
2020-03-18 10:11:14 -0300

    add

--- sw.js
+++ sw.js
@@ -2,46 +2,6 @@ self.addEventListener('fetch', () => {
   // literally does nothing
 });
 
-
-var CACHE_NAME = 'static-v1';
-
-self.addEventListener('install', function (event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      return cache.addAll([
-        '/',
-        `/assets/images/image01.png`,
-        `/assets/main.css`,
-        `/assets/main.js`,
-        `/scripts/main.min.js`,
-      ]);
-    })
-  )
-});
-
-self.addEventListener('activate', function activator(event) {
-  event.waitUntil(
-    caches.keys().then(function (keys) {
-      return Promise.all(keys
-        .filter(function (key) {
-          return key.indexOf(CACHE_NAME) !== 0;
-        })
-        .map(function (key) {
-          return caches.delete(key);
-        })
-      );
-    })
-  );
-});
-
-self.addEventListener('fetch', function (event) {
-  event.respondWith(
-    caches.match(event.request).then(function (cachedResponse) {
-      return cachedResponse || fetch(event.request);
-    })
-  );
-});
-
 importScripts('https://cdn.ampproject.org/sw/amp-sw.js');
 AMP_SW.init({
   assetCachingOpitions: [
@@ -56,6 +16,6 @@ AMP_SW.init({
   ],
   offlinePageOptions: {
     url: '/offline.html',
-    assets: []
+    assets: ['/assets/main.css', '/assets/main.js', '/assets/offline/image01.png', '/assets/offline/image02.png']
   }
 });
