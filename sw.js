ed45d65d1b2330824d1f518420a6daceb7bfd579
Euques <60795401+Euques@users.noreply.github.com>
2020-03-25 12:54:57 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,22 +2,3 @@ self.addEventListener('fetch', () => {
   // literally does nothing
 });
 
-
-
-
-AMP_SW.init({
-  assetCachingOpitions: [
-    {
-      regexp:/\.(png|jpg|svg|webp)/,
-      cachingStrategy: 'CACHE_FIRST'
-  },
-  {
-    regexp: /\.(js|css)/,
-    cachingStrategy: 'STALE_WHILE_REVALIDADATE'
-     }
-  ],
-  offlinePageOptions: {
-    url: '/offline.html',
-    assets: ['/assets/main.css', '/assets/main.js', '/assets/offline/image01.png', '/assets/offline/image02.png']
-  }
-});
